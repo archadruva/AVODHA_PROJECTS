a3b317b3b4c41012617df6654994c6d8952aec67
archadruva <165543003+archadruva@users.noreply.github.com>
2024-04-09 07:18:06 +0530

    Add files via upload

---
+++ program7.py
@@ -0,0 +1,21 @@
+# a=23
+# b=56
+# if b>a:
+#     print("b greater than a")
+
+# x=100
+# y=100
+# if x>y:
+#     print("b greater than a")
+# elif x==y:
+#     print("x and y are equal")
+
+# x=250
+# y=1000
+# if x>y:
+#     print("b greater than a")
+# elif x==y:
+#     print("x and y are equal")
+# else:
+#     print("y greater than x")
+
