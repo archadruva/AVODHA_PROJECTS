54e37602c2208e26135e542d2109db4e8b405e9b
archadruva <165543003+archadruva@users.noreply.github.com>
2024-04-09 07:22:49 +0530

    Add files via upload

---
+++ vehicle.py
@@ -0,0 +1,24 @@
+# class vehicle:
+#     def __init__(self,mileage,fuel):
+#         self.mileage=mileage
+#         self.fuel=fuel
+#     def getdetails(self):
+#         self.mileage=input("enter mileage")
+#         self.fuel=input("enter fuel")
+# class car(vehicle):
+#     def display(self):
+#         print(self.mileage,self.fuel)
+# class bike(car):
+#     def __init__(self, model, color):
+#         self.model=model
+#         self.color=color
+#     def getde(self):
+#         self.model=input("enter model")
+#         self.color=input("enter color")
+#     def putdetails(self):
+#         print(self.model,self.color)
+# obj=bike('','')
+# obj.getdetails()
+# obj.display()
+# obj.getde()
+# obj.putdetails()
