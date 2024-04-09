893c7399ad56a7c5268332fb704d479d92921a0e
archadruva <165543003+archadruva@users.noreply.github.com>
2024-04-09 07:09:06 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,16 @@
+# def my_function():
+#     print("hello from function")
+# my_function()
+
+# def fb(a,b):
+#     return((a/b)**2)
+# x=int(input("enter the price of x"))
+# y=int(input("enter the price of y"))
+# print(fb(x,y))
+
+# def i(a,b,c):
+#     return((a*b*c)/100)
+# x=int(input("enter amt"))
+# y=int(input("enter no of years"))
+# z=int(input("enter rate of interest"))
+# print(i(x,y,z))
