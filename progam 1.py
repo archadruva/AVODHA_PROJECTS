36bf595150e1e72fe1d3da8688ec956de12d2864
archadruva <165543003+archadruva@users.noreply.github.com>
2024-04-09 07:13:02 +0530

    Add files via upload

---
+++ progam 1.py
@@ -0,0 +1,19 @@
+# print("human resources")
+#
+#
+# name=input("enter your name")
+# print(name)
+# place=input("enter your place")
+# print(p
+# a=int(input("enter a number"))
+# p
+# x=["er","qw","yu","ki","oi"]
+# y=[12,"ty","op",67,"op",89]
+# x.extend(y)
+# print(x)
+# x.remove("qw")
+# # print(x)
+# rows=["aa","bb","cc","dd"]
+# print(len(rows))
+# rows.pop(2)
+# print(rows)
