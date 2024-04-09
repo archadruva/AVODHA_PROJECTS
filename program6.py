9be71a1871624332bef44b86f7892f16a8b0d7ee
archadruva <165543003+archadruva@users.noreply.github.com>
2024-04-09 07:17:27 +0530

    Add files via upload

---
+++ program6.py
@@ -0,0 +1,18 @@
+car={"brand":"ford","model":"mustang","year": 1998}
+# x=car.items()
+# print(x)
+# car["year"]=2020
+# print(x)
+# x=car.values()
+# print(x)
+# x=car.keys()
+# x=car["model"]
+# print(x)
+# car.update({"color":"red"})
+# print(car)
+# car.popitem()
+# print(car)
+# car.pop("model")
+# print(car)
+# del car["model"]
+# print(car)
