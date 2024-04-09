84118e88ff7f756b62822f8ffc085e7fcad3cf06
archadruva <165543003+archadruva@users.noreply.github.com>
2024-04-09 07:10:52 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,15 @@
+# x=lambda a: a+10
+# print(x(8))
+
+# x=lambda a,b: a*b
+# print(x(3,7))
+
+# x=lambda a,b,c: a+b+c
+# print(x(3,6,9))
+
+# x=lambda a,b: a/b
+# print(x(3,7))
+
+# x=lambda a,b,c: a*b/c
+# print(x(30,6,9))
+
