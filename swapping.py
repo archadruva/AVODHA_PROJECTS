dd8de4ecd639d752ed13c0e2292f11e0dacfe5f6
archadruva <165543003+archadruva@users.noreply.github.com>
2024-04-09 07:21:38 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,8 @@
+# num1=input("enter a number")
+# num2=input("enter a number")
+
+# num1,num2=num2,num1
+# print("value of num1 after swapping",num1)
+# print("value of num2 after swapping",num2)
+
+
